3d1fb8e216ea1e11eac6776ee48ebf3d151acf18
Pedro Lara <pedroalveslara@gmail.com>
2022-11-03 11:31:45 -0300

    Responsividade

--- wheater-map/src/components/mapa/padrao/index.js
+++ wheater-map/src/components/mapa/padrao/index.js
@@ -7,7 +7,7 @@ const Padrao = () => {
         <div className='row h-100 justify-content-center container-data-padrao'>
             <div className="col-12 text-center m-auto container-padrao">
                 <section>
-                    <p>Clique em cidades do mapa para obter os dados metereológicos!</p>
+                    <p>Clique em cidades do mapa para obter o clima!</p>
                 </section>
             </div>
         </div>
